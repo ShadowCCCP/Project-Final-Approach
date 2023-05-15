b7d9ca71b7cfc9d918c041117d9a8e537ec78948
DariaCristiana24 <dariacristiana24@gmail.com>
2023-05-15 16:15:38 +0200

    Added button class

---
+++ Tech Rat Redemption/Week5+/002_assignment5_starting_code/UIButton.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GXPEngine
+{
+    class UIButton : AnimationSprite
+    {
+        public UIButton() : base("", 1, 1)
+        {
+
+        }
+         
+}
+
+    }
+}
